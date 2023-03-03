f601291e51d50dbb309d81c9314fa14a830aec04
Dav-Cast <dcastillochamorro@gmail.com>
2023-03-02 20:14:56 -0600

    Added Schema and modified csv files.

--- Database_Schema.sql
+++ Database_Schema.sql
@@ -1,22 +1,27 @@
+-- Create table Country_Codes
+CREATE TABLE Country_Codes (
+	Country_ID INT NOT NULL,
+	Country_ID_Alpha VARCHAR(10) NOT NULL,
+	Country_Name VARCHAR(150) NOT NULL,
+	PRIMARY KEY (Country_ID_Alpha)
+);
+
 -- Create table Migrant_Population
 CREATE TABLE Migrant_Population (
 	Country_ID INT NOT NULL,
 	Country_ID_Alpha VARCHAR(10) NOT NULL,
-	Country_Name VARCHAR(50) NOT NULL,
+	Country_Name VARCHAR(150) NOT NULL,
 	Year INT NOT NULL,
 	Total_Migrant_Population INT NOT NULL,
 	Migrant_Male_Population INT NOT NULL,
 	Migrant_Female_Population INT NOT NULL,
-	PRIMARY KEY (Country_ID_Alpha)
+	FOREIGN KEY (Country_ID_Alpha) REFERENCES Country_Codes (Country_ID_Alpha)
 );
-
-DROP TABLE Migrant_Population;
-DROP TABLE Country_Data;
 
 -- Create table Country_Data
 CREATE TABLE Country_Data (
 	Country_ID_Alpha VARCHAR(10) NOT NULL,
-	Country_Name VARCHAR(50) NOT NULL,
+	Country_Name VARCHAR(150) NOT NULL,
 	Year INT NOT NULL,
 	Total_Country_Population INT NOT NULL,
 	Annual_Growth_Rate FLOAT NOT NULL,
@@ -29,6 +34,13 @@ CREATE TABLE Country_Data (
 	Crude_Death_Rate FLOAT NOT NULL,
 	Net_Migration_Rate FLOAT NOT NULL,
 	Migration_Flag BOOLEAN NOT NULL,
-	FOREIGN KEY (Country_ID_Alpha) REFERENCES Migrant_Population (Country_ID_Alpha),
-	PRIMARY KEY (Country_ID_Alpha)
+	FOREIGN KEY (Country_ID_Alpha) REFERENCES Country_Codes (Country_ID_Alpha)
 );
+
+DROP TABLE Country_Codes;
+DROP TABLE Migrant_Population;
+DROP TABLE Country_Data;
+
+SELECT * FROM country_codes;
+SELECT * FROM migrant_population;
+SELECT * FROM country_data;
