10cdd4c136f668659df72b538912a6aa54760cb3
rrantxa <113153777+rrantxa@users.noreply.github.com>
2023-03-02 20:25:22 -0600

    Updating ERD.

--- ERD/QuickDBD-export.sql
+++ ERD/QuickDBD-export.sql
@@ -2,46 +2,48 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-SET XACT_ABORT ON
+CREATE TABLE "Country_Codes" (
+    "Country_ID" int   NOT NULL,
+    "Country_ID_Alpha" varchar   NOT NULL,
+    "Country_Name" varchar   NOT NULL,
+    CONSTRAINT "pk_Country_Codes" PRIMARY KEY (
+        "Country_ID_Alpha"
+     )
+);
 
-BEGIN TRANSACTION QUICKDBD
+CREATE TABLE "Migrant_Population" (
+    "Country_ID" int   NOT NULL,
+    "Country_ID_Alpha" varchar   NOT NULL,
+    "Country_Name" varchar   NOT NULL,
+    "Year" int   NOT NULL,
+    "Total_Migrant_Population" int   NOT NULL,
+    "Migrant_Male_Population" int   NOT NULL,
+    "Migrant_Female_Population" int   NOT NULL
+);
 
-CREATE TABLE [Migrant_Population] (
-    [Country_ID] int  NOT NULL ,
-    [Country_ID_Alpha] varchar  NOT NULL ,
-    [Country_Name] varchar  NOT NULL ,
-    [Year] int  NOT NULL ,
-    [Total_Migrant_Population] int  NOT NULL ,
-    [Migrant_Male_Population] int  NOT NULL ,
-    [Migrant_Femaile_Population] int  NOT NULL ,
-    CONSTRAINT [PK_Migrant_Population] PRIMARY KEY CLUSTERED (
-        [Country_ID_Alpha] ASC
-    )
-)
+CREATE TABLE "Country_Data" (
+    "Country_ID_Alpha" varchar   NOT NULL,
+    "Country_Name" varchar   NOT NULL,
+    "Year" int   NOT NULL,
+    "Total_Country_Population" int   NOT NULL,
+    "Annual_Growth_Rate" float   NOT NULL,
+    "Country_Area" int   NOT NULL,
+    "Population_Density" float   NOT NULL,
+    "Total_Fertility_Rate" float   NOT NULL,
+    "Crude_Birth_Rate" float   NOT NULL,
+    "Life_Expectancy_at_Birth" float   NOT NULL,
+    "Infant_Mortality_Rate" float   NOT NULL,
+    "Crude_Death_Rate" float   NOT NULL,
+    "Net_Migration_Rate" float   NOT NULL,
+    "Migration_Flag" bool   NOT NULL
+);
 
-CREATE TABLE [Country_Data] (
-    [Country_ID_Alpha] varchar  NOT NULL ,
-    [Country_Name] varchar  NOT NULL ,
-    [Year] int  NOT NULL ,
-    [Total_Country_Population] int  NOT NULL ,
-    [Annual_Growth_Rate] float  NOT NULL ,
-    [Country_Area] int  NOT NULL ,
-    [Population_Density] float  NOT NULL ,
-    [Total_Fertility_Rate] float  NOT NULL ,
-    [Crude_Birth_Rate] float  NOT NULL ,
-    [Life_Expectancy_at_Birth] float  NOT NULL ,
-    [Infant_Mortality_Rate] float  NOT NULL ,
-    [Crude_Death_Rate] float  NOT NULL ,
-    [Net_Migration_Rate] float  NOT NULL ,
-    [Migration_Flag] bool  NOT NULL ,
-    CONSTRAINT [PK_Country_Data] PRIMARY KEY CLUSTERED (
-        [Country_ID_Alpha] ASC
-    )
-)
+ALTER TABLE "Country_Codes" ADD CONSTRAINT "fk_Country_Codes_Country_ID" FOREIGN KEY("Country_ID")
+REFERENCES "Migrant_Population" ("Country_ID");
 
-ALTER TABLE [Migrant_Population] WITH CHECK ADD CONSTRAINT [FK_Migrant_Population_Country_ID_Alpha_Year] FOREIGN KEY([Country_ID_Alpha], [Year])
-REFERENCES [Country_Data] ([Country_ID_Alpha], [Year])
+ALTER TABLE "Country_Codes" ADD CONSTRAINT "fk_Country_Codes_Country_ID_Alpha" FOREIGN KEY("Country_ID_Alpha")
+REFERENCES "Country_Data" ("Country_ID_Alpha");
 
-ALTER TABLE [Migrant_Population] CHECK CONSTRAINT [FK_Migrant_Population_Country_ID_Alpha_Year]
+ALTER TABLE "Migrant_Population" ADD CONSTRAINT "fk_Migrant_Population_Country_ID_Alpha" FOREIGN KEY("Country_ID_Alpha")
+REFERENCES "Country_Codes" ("Country_ID_Alpha");
 
-COMMIT TRANSACTION QUICKDBD
